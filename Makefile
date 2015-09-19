48388790b9dae5ed5d93b36d1b776432c6ca8c5c
Nikita Leshenko <nikita@leshenko.net>
2015-09-20 00:39:21 +0300

    Spinnig cube just to test things

--- Makefile
+++ Makefile
@@ -1,9 +1,15 @@
-.PHONY: server
+.PHONY: server watch clean
 
 TS_FILES := $(shell find src/ -type f)
 
 public/js/clock.js: src/main.ts $(TS_FILES)
-	tsc --watch --out $@ $<
+	tsc $(TSC_FLAGS) --out $@ $<
 
 server:
 	(cd public && python -m SimpleHTTPServer 8989)
+
+watch:
+	$(MAKE) TSC_FLAGS=--watch
+
+clean:
+	-rm -f public/js/clock.js
--- src/main.ts
+++ src/main.ts
@@ -9,10 +9,10 @@ function main ()
     renderer.setSize (window.innerWidth, window.innerHeight);
     document.body.appendChild (renderer.domElement);
 
-    var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-    var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-    var cube = new THREE.Mesh( geometry, material );
-    scene.add( cube );
+    var geometry = new THREE.BoxGeometry (1, 1, 1);
+    var material = new THREE.MeshBasicMaterial ({ color: 0x00ff00 });
+    var cube = new THREE.Mesh (geometry, material);
+    scene.add (cube);
 
     camera.position.z = 5;
     function render ()
@@ -23,7 +23,7 @@ function main ()
         renderer.render (scene, camera);
     }
 
-    render();
+    render ();
 }
 
 main ();
