6af5202035842c44d2ef72b973a86818ed3087d8
Nikita Leshenko <nikita@leshenko.net>
2015-10-14 00:41:09 +0300

    Fix camera again

--- src/math.ts
+++ src/math.ts
@@ -7,3 +7,8 @@ function floor_to (n:number, to:number):number
 {
     return Math.floor (n / to) * to;
 }
+
+function mod (n:number, mod:number):number
+{
+    return (n % mod + mod) % mod;
+}
--- src/orbit_camera.ts
+++ src/orbit_camera.ts
@@ -22,14 +22,14 @@ class Orbit_camera extends THREE.PerspectiveCamera
     public rotate (horizontal:number, vertical:number)
     {
         this.vertical += vertical;
-        this.vertical = Math.max (-Math.PI, Math.min (Math.PI, this.vertical));
+        this.vertical = mod (this.vertical + Math.PI, Math.PI * 2) - Math.PI;
 
         if (Math.abs (this.vertical) < Math.PI / 2)
         {
             horizontal = -horizontal;
         }
         this.horizontal += horizontal;
-        this.horizontal = Math.max (-Math.PI, Math.min (Math.PI, this.horizontal));
+        this.horizontal = mod (this.horizontal + Math.PI, Math.PI * 2) - Math.PI;
     }
 
     public pan (x:number, y:number)
